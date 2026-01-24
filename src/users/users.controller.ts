e2574ff99cba56d46cedcf5cd03c923ecd836fbd
BiggieOneBillion <chukwuchinwendu94@gmail.com>
2026-01-24 05:34:42 +0100

    docs(users): add missing swagger imports to users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,7 +12,14 @@ import {
   NotFoundException,
   Put,
 } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
 import { UsersService } from './users.service';
+
 import { CreateAdminDetailsDto, CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
