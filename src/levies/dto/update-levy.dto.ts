98a0a4d5e0d7e465419811338aa3f47cfc958117
BiggieOneBillion <chukwuchinwendu94@gmail.com>
2026-01-27 18:57:22 +0100

    docs: add swagger annotations to levies dtos

--- src/levies/dto/update-levy.dto.ts
+++ src/levies/dto/update-levy.dto.ts
@@ -1,8 +1,9 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType, ApiPropertyOptional } from '@nestjs/swagger';
 import { CreateLevyDto } from './create-levy.dto';
 import { IsBoolean, IsOptional } from 'class-validator';
 
 export class UpdateLevyDto extends PartialType(CreateLevyDto) {
+  @ApiPropertyOptional({ description: 'Active status of the levy', default: true })
   @IsBoolean()
   @IsOptional()
   isActive?: boolean;
